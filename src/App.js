badedaf3e07eee848137a3050c39e00aa62c8218
KenzheSan <js3nursultan@gmial.com>
2022-01-25 17:41:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import AddBookForm from './components/AdminAddBook/AddBookForm/AddBookForm';
-// import Example from './components/example';
-// import Example from './components/example';
-// import NewPage from './components/index3';
-// import Example from './components';
 
 function App() {
   return (
     <div className="App">
-      {/* {<NewPage/>} */}
-      {/* <Example/> */}
-      {/* <Example/> */}
-      <AddBookForm/>
+
     </div>
   );
 }
